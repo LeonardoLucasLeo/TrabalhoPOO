88963269ffbe42c1319d8360b9254331009af7f5
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2017-07-10 21:13:56 -0300

    Update Simulador_de_Batalha.java

--- Simulador_de_Batalha.java
+++ Simulador_de_Batalha.java
@@ -18,6 +18,7 @@ public class Simulador_de_Batalha {
         
         Npc npc1 = new Npc(31, "Vendedor");
         Heroi heroi1 = new Heroi(6, "Diablo", 666, 10);
+        Heroi heroi2 = new Heroi(7, "Varian", 80, 8)
         heroi1.imprimir();
         
         Vilao vilao1 = new Vilao(5, "Minion");
